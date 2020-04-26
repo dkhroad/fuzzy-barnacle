14adea61cc4ebda826fb20b3e1e40782edff2d64
Devinder Khroad <dkhroad@gmail.com>
2020-04-26 11:35:03 -0700

    prob1 tests passing

--- prob1/validate_pull_request.sh
+++ prob1/validate_pull_request.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+JIRA_REGEX="\[JIRA-\d+\]"
+
 check_for_jira_issue() {
-  return 1
+  local message=$1
+  if  echo $message | grep -iqE $JIRA_REGEX ; then
+    return 0 
+  else
+    echo "ERROR: Pull request title must contain a JIRA issue number"
+    return 1
+  fi
 }
+
+
