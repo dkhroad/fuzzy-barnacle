f2f00a5c0282a2d7867172eb3adb147af540506f
Devinder Khroad <dkhroad@gmail.com>
2020-04-26 17:53:32 -0700

    prob1: added some comments

--- prob1/validate_pull_request.sh
+++ prob1/validate_pull_request.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# A shell script to check if the given string
+# cantains a sub string in the form of [JIRA-<number]. 
+# where <number> as string of digits. For example: [JIRA-124578]
+# 
+
 JIRA_REGEX="\[JIRA-\d+\]"
+
+# returns 0 if the the string provided as the first argument contains
+# a sub string with JIRA issue number in square brackets.
+# for example:
+# check_for_jira_issue "[JIRA-1234] a pull request title with a valid  jira issue"
+
 
 check_for_jira_issue() {
   local message=$1
